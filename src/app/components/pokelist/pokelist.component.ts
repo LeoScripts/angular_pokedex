9dc517b6e78eb2d07f12ae364ede9c11e8047276
Leandro Cavalcante <leandroguitarjesus89@gmail.com>
2024-06-06 15:35:46 -0300

    feat: add pokelist component

---
+++ src/app/components/pokelist/pokelist.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokelist',
+  standalone: true,
+  imports: [],
+  templateUrl: './pokelist.component.html',
+  styleUrl: './pokelist.component.scss'
+})
+export class PokelistComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { PokelistComponent } from '../../components/pokelist/pokelist.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, PokelistComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
