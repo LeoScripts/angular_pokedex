04807bb2077710287bb1d3fba827d47bbe5b6509
Leandro Cavalcante <leandroguitarjesus89@gmail.com>
2024-06-07 18:50:12 -0300

    feat: integrate poke list with card

--- src/app/components/pokecard/pokecard.component.ts
+++ src/app/components/pokecard/pokecard.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Poke } from '../pokelist/pokelist.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pokecard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './pokecard.component.html',
-  styleUrl: './pokecard.component.scss'
+  styleUrl: './pokecard.component.scss',
 })
 export class PokecardComponent {
+  @Input() poke: Poke | any;
 
+  public addZero(str: string | number, size: number): string {
+    let s = String(str);
+
+    while (s.length < (size || 2)) {
+      s = '0' + s;
+    }
+    return s;
+  }
 }
--- src/app/components/pokelist/pokelist.component.ts
+++ src/app/components/pokelist/pokelist.component.ts
@@ -1,11 +1,61 @@
 import { Component } from '@angular/core';
 import { PokecardComponent } from '../pokecard/pokecard.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pokelist',
   standalone: true,
-  imports: [PokecardComponent],
+  imports: [CommonModule, PokecardComponent],
   templateUrl: './pokelist.component.html',
   styleUrl: './pokelist.component.scss',
 })
-export class PokelistComponent {}
+export class PokelistComponent {
+  pokes: Poke[] = [
+    {
+      id: 1,
+      img: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/153.png',
+      name: 'tiranosauro rex',
+      types: [Type.dragon, Type.poison],
+    },
+    {
+      id: 1,
+      img: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/153.png',
+      name: 'tiranosauro rex',
+      types: [Type.dragon, Type.poison],
+    },
+    {
+      id: 1,
+      img: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/153.png',
+      name: 'tiranosauro rex',
+      types: [Type.dragon, Type.poison],
+    },
+  ];
+}
+
+export interface Poke {
+  id: number;
+  name: string;
+  img: string;
+  types: Type[];
+}
+
+enum Type {
+  bug = 'bug',
+  dark = 'dark',
+  dragon = 'dragon',
+  electric = 'electric',
+  fairy = 'fairy',
+  fighting = 'fighting',
+  fire = 'fire',
+  flying = 'flying',
+  ghost = 'ghost',
+  grass = 'grass',
+  ground = 'ground',
+  ice = 'ice',
+  normal = 'normal',
+  poison = 'poison',
+  psychic = 'psychic',
+  rock = 'rock',
+  steel = 'steel',
+  water = 'water',
+}
