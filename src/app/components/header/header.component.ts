5b5ec714015f20491669aa5c7bd5a5ce4a94112e
Leandro Cavalcante <leandroguitarjesus89@gmail.com>
2024-06-08 22:35:10 -0300

    refactor: change intregration to api

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-
+  onInput(event: any) {
+    const inputValue = event.target.value;
+  }
 }
--- src/app/poke.service.ts
+++ src/app/poke.service.ts
@@ -3,34 +3,30 @@ import { Injectable } from '@angular/core';
 import { Poke } from './components/pokelist/pokelist.component';
 import { ReplaySubject, from, map, mergeMap } from 'rxjs';
 
+export let offset: number = 1;
+export let limit: number = 30;
+export let param: string | number = '';
+const baseUrl = `https://pokeapi.co/api/v2/pokemon`;
+const paginate = `?offset=${offset}&limit=${limit}`;
+
+console.log(baseUrl + paginate);
 @Injectable({
   providedIn: 'root',
 })
 export class PokeService {
   public pokes: Poke[] = [];
   constructor(public httpClient: HttpClient) {
-    let offset: number = 0;
-    let limit: number = 30;
-    const pokeapiUrl = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
-    this.httpClient
-      .get<any>(pokeapiUrl)
-      .pipe(
-        map((values) => values.results),
-        map((value: any) => {
-          return from(value).pipe(
-            mergeMap((v: any) => this.httpClient.get(v.url))
-          );
-        }),
-        mergeMap((value) => value)
-      )
-      .subscribe((result: any) => {
-        const poke: Poke = {
-          id: result.id,
-          name: result.name,
-          img: result.sprites.other.dream_world.front_default,
-          types: result.types.map((v: any) => v.type.name),
-        };
-        this.pokes[result.id] = poke;
-      });
+    this.getPokes(offset, limit);
+  }
+
+  async getPokes(offset: number, limit: number) {
+    const response = await this.httpClient
+      .get<any>(baseUrl + paginate)
+      .toPromise();
+    let data: any = [];
+    for (let i = offset; i < response.results.length + offset; i++) {
+      data.push(await this.httpClient.get<any>(`${baseUrl}/${i}`).toPromise());
+    }
+    this.pokes = data;
   }
 }
